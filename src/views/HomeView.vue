f907697f9d8803cdcfdad73b95316dbfe5b6b2f8
Robinson Berthet <berthet.robinson@gmail.com>
2025-02-06 10:06:14 +0100

    test otp

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,29 +5,26 @@
       Page d'accueil test1
     </p>
   </div>
-  <!-- <CameraVue />
-  <PositionVue />
-  <BatteriePower /> -->
+
   <SmsOTP />
-  <!-- <PhoneCall /> -->
 
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import CameraVue from '@/components/CameraVue.vue'; // @ is an alias to /src
-import PositionVue from '@/components/PositionVue.vue';
-import BatteriePower from '@/components/BatteriePower.vue';
-import PhoneCall from '@/components/PhoneCall.vue';
+// import CameraVue from '@/components/CameraVue.vue'; // @ is an alias to /src
+// import PositionVue from '@/components/PositionVue.vue';
+// import BatteriePower from '@/components/BatteriePower.vue';
+// import PhoneCall from '@/components/PhoneCall.vue';
 import SmsOTP from '@/components/SmsOTP.vue';
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    CameraVue,
-    PositionVue,
-    BatteriePower,
-    PhoneCall,
+    // CameraVue,
+    // PositionVue,
+    // BatteriePower,
+    // PhoneCall,
     SmsOTP,
   },
 });
